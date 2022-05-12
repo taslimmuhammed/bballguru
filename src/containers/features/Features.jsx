9de600c7e8e96aa4e320dec54120143aa611e5e3
taslimmuhammed <taslimmuhammed67@gmail.com>
2022-05-12 14:35:02 +0530

    added text

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -7,8 +7,8 @@ const Features = () => (
  <div className='features_main' id='roadmap'>
   <div className="gpt3__features section__padding" id="features">
     <div className="gpt3__features-heading">
-      <h1 className="gradient__text">consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</h1>
-      <p>Request Early Access to Get Started</p>
+      <h1 className="gradient__text">Why An NFT Book Collection?</h1>
+      <p>Most People own Physical and digital Ebooks. Although, only physical books have a resale value. The Ebooks and audiobook technically do NOT belong to us. We only pay a one time access fee to the literature. With the evolution of technology, now books can be stored on the blockchain as an NFT. I am now selling my book Gentritrying on the blockchain as an NFT, along with an exclusive AR book cover and much much more</p>
     </div>
     <div className="gpt3__features-container">
      <div>
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -8,10 +8,12 @@ const Possibility = () => (
       <img src={Book_Cover} alt="possibility"  className='gpt3__possibility_img' />
     </div>
     <div className="gpt3__possibility-content">
-      <h4>Lorem ipsum began as scrambled, nonsensical </h4>
-      <h1 className="gradient__text">Lorem ipsum dolor sit <br /> elit, sed do eiusmod tempor</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-      <h4>ipsum dolor sit amet, consectetur adipiscing el</h4>
+      <h4> </h4>
+      <h1 className="gradient__text">Why An NFT Book Collection?</h1>
+      <p>New non-fungible token (NFT) collections pop up every other day, offering collectors an opportunity to become the first buyer of an NFT – a process known as “minting.”
+When it comes to navigating NFTs, minting as a term may seem confusing because it refers both to a collection becoming part of the blockchain and to a collector buying an item from that collection for the first time. We will focus on minting in the latter sense.
+One of the main benefits of minting NFTs is investors are able to buy into a collection at the earliest possible opportunity. That means they stand the best chance of making a profit. As far as writing goes; NFTs are a way for experimental writers to experiment with form especially writers whose interests lie in technology. The Gentritrying NFT collection, will revolutionize the way Authors can make their voices heard through literature and now technology.  </p>
+      <h4>ABOUT AUTHOR: A marketing manager with 3+ years of community management and experience for multiple startups in the cryptocurrency sector. Gabriel has been founder of numerous web3 and non profit start ups. He Has been minting, researching, and earning certifications in the cryptocurrency, NFT, blockchain, AR space for the last 10 years. He is an active member in over 5k Discord channels and helps build communities within NFT spaces (has been active on Discord since the launch of the platform). Gabriel is an avid crypto, blockchain, web3, and DAO enthusiast who has minted NFTs on 5 different platforms using Artificial Intelligence art. He has an extensive experience building and nurturing online communities organically. He has also in the past worked in the inner city building communities as well. Most notably a teacher, basketball coach and mentor in NYC. Gabriel is also the Author of “Gentritrying”, a NFT book that focuses on gentrification in minority neighborhoods all across America. </h4>
     </div>
   </div>
 );
